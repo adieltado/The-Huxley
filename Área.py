e2b3274aec760dbe61b998c2649e7068b19168cf
Adiel Alves <109315792+adieltado@users.noreply.github.com>
2022-08-06 02:55:40 -0300

    2805 - Área do trapézio

---
+++ Área.py
@@ -0,0 +1,10 @@
+baseMaior = int(input())
+baseMenor = int(input())
+altura = int(input())
+
+A = (((baseMaior+baseMenor)*altura)/2)
+
+print('Digite a base menor:')
+print('Digite a base maior:')
+print('Digite a altura:')
+print('Area do trapezio: ' "{:.6f}".format(A))
