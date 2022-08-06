a389c9e1e5a0815cf02edfcbdb25326d40cc9452
Adiel Alves <109315792+adieltado@users.noreply.github.com>
2022-08-06 03:09:06 -0300

    Rename Resposta.py to 3579 - Resposta bem explicadinha

---
+++ 3579 - Resposta bem explicadinha
@@ -0,0 +1,5 @@
+x1 = int(input())
+x2 = int(input())
+p = x1*x2
+
+print('O produto entre %i  e %i é dado por: %i' %(x1, x2, p))
--- Resposta.py
+++
@@ -1,5 +0,0 @@
-x1 = int(input())
-x2 = int(input())
-p = x1*x2
-
-print('O produto entre %i  e %i é dado por: %i' %(x1, x2, p))
